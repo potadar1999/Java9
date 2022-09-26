5a99dfe92c8603c831ad6cdf1611db58412c6628
Sandhya Potadar <101388965+potadar1999@users.noreply.github.com>
2022-09-26 19:53:23 +0530

    Add files via upload

---
+++ PlayList.java
@@ -0,0 +1,15 @@
+package com.secondQue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayList {
+	
+	 public List<Song> songs = new ArrayList<>();
+	 
+	 public void addSong(Song song) {
+
+//		 song.add
+	 }
+	 
+}
